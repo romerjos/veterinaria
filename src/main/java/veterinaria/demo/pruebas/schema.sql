43f4be245b834dcbdd352541857a7afd6108ce6f
Romer J <romer.jos.02@gmail.com>
2026-03-27 14:51:33 -0500

    db: crear tabla de pacientes

---
+++ src/main/java/veterinaria/demo/pruebas/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE pacientes (
+                           id INT AUTO_INCREMENT PRIMARY KEY,
+                           nombre VARCHAR(100),
+                           especie VARCHAR(50),
+                           edad INT
+);
