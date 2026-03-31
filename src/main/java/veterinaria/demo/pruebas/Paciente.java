aaa2fd639a03960c34e141dcea243a61c5877783
Romer J <romer.jos.02@gmail.com>
2026-03-31 07:52:56 -0500

    cambios nuevos

--- src/main/java/veterinaria/demo/pruebas/Paciente.java
+++ src/main/java/veterinaria/demo/pruebas/Paciente.java
@@ -4,16 +4,19 @@ public class Paciente {
     private String nombre;
     private String especie;
     private int edad;
+    private String propietario;
 
     public Paciente(String nombre, String especie, int edad) {
         this.nombre = nombre;
         this.especie = especie;
         this.edad = edad;
+        this.propietario = propietario;
     }
 
     public void mostrarInfo() {
         System.out.println("Nombre: " + nombre);
         System.out.println("Especie: " + especie);
         System.out.println("Edad: " + edad);
+        System.out.println("Propietario: " + propietario);
     }
 }
